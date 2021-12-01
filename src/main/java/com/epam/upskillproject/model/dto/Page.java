c15897ad60c54933ea7631fd2abb166c496cb6bc
pmiakish <mvecktor@gmail.com>
2021-12-01 11:00:02 +0300

    Add equals(), hashCode() to Page

--- src/main/java/com/epam/upskillproject/model/dto/Page.java
+++ src/main/java/com/epam/upskillproject/model/dto/Page.java
@@ -2,6 +2,7 @@ package com.epam.upskillproject.model.dto;
 
 import com.epam.upskillproject.model.dao.queryhandlers.sqlorder.sort.SortType;
 import java.util.List;
+import java.util.Objects;
 
 public class Page<T> {
     private final List<T> entries;
@@ -36,5 +37,33 @@ public class Page<T> {
 
     public String getSort() {
         return sortType.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Page<?> page = (Page<?>) o;
+        return pageNumber == page.pageNumber &&
+                pageSize == page.pageSize &&
+                total == page.total &&
+                Objects.equals(entries, page.entries) &&
+                Objects.equals(sortType, page.sortType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(entries, pageNumber, pageSize, total, sortType);
+    }
+
+    @Override
+    public String toString() {
+        return "Page{" +
+                "entries=" + entries +
+                ", pageNumber=" + pageNumber +
+                ", pageSize=" + pageSize +
+                ", total=" + total +
+                ", sortType=" + sortType +
+                '}';
     }
 }
